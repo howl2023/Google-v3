517421bf1ffd73244ef4ff1afaf62baceecd5235
Howl Lee <929464039@qq.com>
2023-04-08 12:20:10 +0800

    install nextjs and tailwindCSS

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <title>Google V3</title>
+      <meta name="description" content='google' />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
