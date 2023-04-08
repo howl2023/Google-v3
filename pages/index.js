909adfb45fe02e79d656346bf2bf57fee861cb79
Howl Lee <929464039@qq.com>
2023-04-08 13:39:56 +0800

    create the header section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      Hello,world
+      <Head>
+        <title>Google V3</title>
+        <meta name="description" content='google' />
+        <link ref="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header */}
+      <Header />
+      {/* Body */}
+
+      {/* {Footer} */}
     </div>
   )
 }
